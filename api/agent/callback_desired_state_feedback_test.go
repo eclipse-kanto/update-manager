212e8f18b8cc087deaee481b7a614ab21922b4b9
Ivan Boev <68280294+ivan-boev@users.noreply.github.com>
2023-07-24 11:52:12 +0300

    Add unit tests for agent, agent_options, callback_current_state, callback_desired_state_feedback (#12)
    [#15] Add unit tests for agent, agent_options, callback_current_state, callback_desired_state_feedback
    ---------
    
    Signed-off-by: Boev Ivan <Ivan.Boev@bosch.com>

--- api/agent/callback_desired_state_feedback_test.go
+++ api/agent/callback_desired_state_feedback_test.go
@@ -15,12 +15,18 @@ package agent
 import (
 	"errors"
 	"testing"
+	"time"
 
 	"github.com/eclipse-kanto/update-manager/api/types"
 	"github.com/eclipse-kanto/update-manager/test/mocks"
 
 	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
 )
+
+var actions = []*types.Action{{
+	Component: &types.Component{ID: "mydomain:mycomponent", Version: "1.2.3"},
+}}
 
 func TestHandleDesiredStateFeedbackEvent(t *testing.T) {
 	mockCtr := gomock.NewController(t)
@@ -46,13 +52,7 @@ func TestHandleDesiredStateFeedbackEvent(t *testing.T) {
 		"test_feedback_with_actions": {
 			status:  types.StatusIdentified,
 			message: "actions identified",
-			actions: []*types.Action{
-				{
-					Component: &types.Component{ID: "mydomain:mycomponent", Version: "1.2.3"},
-					Status:    types.ActionStatusIdentified,
-					Message:   "actions identified",
-				},
-			},
+			actions: configureActions(types.ActionStatusIdentified, "actions identified"),
 		},
 		"test_feedback_with_actions_send_error": {
 			status:  types.StatusIncompleteInconsistent,
@@ -85,4 +85,121 @@ func TestHandleDesiredStateFeedbackEvent(t *testing.T) {
 			updAgent.HandleDesiredStateFeedbackEvent("", testActivityID, "", test.status, test.message, test.actions)
 		})
 	}
+
+	t.Run("test_feedback_dsFeedbackNotifier_not_nil", func(t *testing.T) {
+		updAgent := &updateAgent{
+			client: mockClient,
+		}
+		mockClient.EXPECT().SendDesiredStateFeedback(gomock.Any(), gomock.Any()).DoAndReturn(func(activityID string, feedback *types.DesiredStateFeedback) error {
+			expectedFeedback := &types.DesiredStateFeedback{
+				Status:  types.StatusCompleted,
+				Message: "operation completed",
+				Actions: configureActions(types.ActionStatusUpdateSuccess, "update success"),
+			}
+			assert.Equal(t, testActivityID, activityID)
+			assert.Equal(t, expectedFeedback, feedback)
+			return nil
+		})
+		dsNotifier := &desiredStateFeedbackNotifier{
+			internalTimer: time.AfterFunc(time.Millisecond, nil),
+		}
+		updAgent.desiredStateFeedbackNotifier = dsNotifier
+		updAgent.HandleDesiredStateFeedbackEvent("", testActivityID, "", types.StatusCompleted, "operation completed", actions)
+
+		assert.Nil(t, updAgent.desiredStateFeedbackNotifier.internalTimer)
+
+		updAgent.desiredStateFeedbackNotifier = nil
+	})
+
+	t.Run("test_feedback_interval_invalid", func(t *testing.T) {
+		updAgent := &updateAgent{
+			client: mockClient,
+		}
+		updAgent.desiredStateFeedbackReportInterval = -1 * time.Second
+		updAgent.HandleDesiredStateFeedbackEvent("", testActivityID, "", types.StatusRunning, "operation running", actions)
+
+		assert.Nil(t, updAgent.desiredStateFeedbackNotifier)
+	})
+
+	t.Run("test_feedback_actions_status_running_no_err", func(t *testing.T) {
+		updAgent := &updateAgent{
+			client: mockClient,
+		}
+		mockClient.EXPECT().SendDesiredStateFeedback(gomock.Any(), gomock.Any()).DoAndReturn(func(activityID string, feedback *types.DesiredStateFeedback) error {
+			expectedFeedback := &types.DesiredStateFeedback{
+				Status:  types.StatusRunning,
+				Message: "",
+				Actions: configureActions(types.ActionStatusDownloading, "downloading"),
+			}
+			assert.Equal(t, testActivityID, activityID)
+			assert.Equal(t, expectedFeedback, feedback)
+			return nil
+		})
+		updAgent.desiredStateFeedbackReportInterval = interval
+		updAgent.HandleDesiredStateFeedbackEvent("", testActivityID, "", types.StatusRunning, "operation running", actions)
+		updAgent.desiredStateFeedbackNotifier.internalTimer.Stop()
+	})
+
+	t.Run("test_feedback_actions_status_running_multiple_events_timer_recreation_ok", func(t *testing.T) {
+		updAgent := &updateAgent{
+			client: mockClient,
+		}
+		mockClient.EXPECT().SendDesiredStateFeedback(gomock.Any(), gomock.Any()).DoAndReturn(func(activityID string, feedback *types.DesiredStateFeedback) error {
+			expectedFeedback := &types.DesiredStateFeedback{
+				Status:  types.StatusRunning,
+				Message: "",
+				Actions: configureActions(types.ActionStatusDownloading, "downloading"),
+			}
+			assert.Equal(t, testActivityID, activityID)
+			assert.Equal(t, expectedFeedback, feedback)
+			return nil
+		})
+		updAgent.desiredStateFeedbackReportInterval = interval
+		updAgent.HandleDesiredStateFeedbackEvent("", testActivityID, "", types.StatusRunning, "operation running", actions)
+		timer1 := updAgent.desiredStateFeedbackNotifier.internalTimer
+		updAgent.HandleDesiredStateFeedbackEvent("", testActivityID, "", types.StatusRunning, "operation running", actions)
+		timer2 := updAgent.desiredStateFeedbackNotifier.internalTimer
+		assert.Equal(t, timer1, timer2)
+		updAgent.desiredStateFeedbackNotifier.internalTimer.Stop()
+	})
+
+	t.Run("test_status_incomplete", func(t *testing.T) {
+		updAgent := &updateAgent{
+			client: mockClient,
+		}
+		mockClient.EXPECT().SendDesiredStateFeedback(gomock.Any(), gomock.Any()).DoAndReturn(func(activityID string, feedback *types.DesiredStateFeedback) error {
+			expectedFeedback := &types.DesiredStateFeedback{
+				Status:  types.StatusIncomplete,
+				Message: "downloading",
+				Actions: configureActions(types.ActionStatusDownloading, "downloading"),
+			}
+			assert.Equal(t, testActivityID, activityID)
+			assert.Equal(t, expectedFeedback, feedback)
+			return nil
+		})
+		updAgent.HandleDesiredStateFeedbackEvent("", testActivityID, "", types.StatusIncomplete, "downloading", actions)
+	})
+
+	t.Run("test_status_identifying", func(t *testing.T) {
+		updAgent := &updateAgent{
+			client: mockClient,
+		}
+		mockClient.EXPECT().SendDesiredStateFeedback(gomock.Any(), gomock.Any()).DoAndReturn(func(activityID string, feedback *types.DesiredStateFeedback) error {
+			expectedFeedback := &types.DesiredStateFeedback{
+				Status:  types.StatusIdentifying,
+				Message: "identifying",
+				Actions: configureActions(types.ActionStatusActivating, "activating"),
+			}
+			assert.Equal(t, testActivityID, activityID)
+			assert.Equal(t, expectedFeedback, feedback)
+			return nil
+		})
+		updAgent.HandleDesiredStateFeedbackEvent("", testActivityID, "", types.StatusIdentifying, "identifying", actions)
+	})
+}
+
+func configureActions(status types.ActionStatusType, message string) []*types.Action {
+	actions[0].Status = status
+	actions[0].Message = message
+	return actions
 }
